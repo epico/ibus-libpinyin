1c9a7295dcc1da382bf83a516f39a0e36ccfc4c8
Peng Wu <alexepico@gmail.com>
2012-06-06 13:45:12 +0800

    update src

--- src/PYSimpTradConverter.cc
+++ src/PYSimpTradConverter.cc
@@ -1,6 +1,6 @@
 /* vim:set et ts=4 sts=4:
  *
- * ibus-pinyin - The Chinese PinYin engine for IBus
+ * ibus-libpinyin - Intelligent Pinyin engine based on libpinyin for IBus
  *
  * Copyright (c) 2008-2010 Peng Huang <shawn.p.huang@gmail.com>
  * Copyright (c) 2010 BYVoid <byvoid1@gmail.com>
