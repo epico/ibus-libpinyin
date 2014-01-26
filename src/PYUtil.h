aba1516229c018439459e0e20995e91c3aa6ea53
Peng Wu <alexepico@gmail.com>
2014-01-26 14:58:27 +0800

    drop uuid deps

--- src/PYUtil.h
+++ src/PYUtil.h
@@ -25,13 +25,7 @@
 #  include <config.h>
 #endif
 
-#if defined(HAVE_UUID_CREATE)
-#  include <uuid.h>
-#elif defined(HAVE_LIBUUID)
-#  include <uuid/uuid.h>
-#endif
 
-#include <sys/utsname.h>
 #include <cstdlib>
 #include <string>
 
@@ -89,32 +83,7 @@ scmshm_test (guint modifiers, guint mask)
     return scmshm_filter (modifiers) == mask;
 }
 
-class UUID {
-public:
-    UUID (void)
-    {
-        uuid_t u;
-#if defined(HAVE_UUID_CREATE)
-        gchar* uuid;
-        uuid_create (&u, 0);
-        uuid_to_string (&u, &uuid, 0);
-        g_strlcpy (m_uuid, uuid, sizeof(m_uuid));
-        free(uuid);
-#elif defined(HAVE_LIBUUID)
-        uuid_generate (u);
-        uuid_unparse_lower (u, m_uuid);
-#endif
-    }
-
-    operator const gchar * (void) const
-    {
-        return m_uuid;        
-    }
-
-private:
-    gchar m_uuid[256];
-};
-
+#if 0
 class Uname {
 public:
     Uname (void)
@@ -149,6 +118,7 @@ public:
         return c_str();
     }
 };
+#endif
 
 };
 #endif
