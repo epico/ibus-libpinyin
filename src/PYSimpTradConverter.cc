b04d6793de65fa815737b5eb9a1db92a74bcf81f
Peng Wu <alexepico@gmail.com>
2014-12-25 13:10:57 +0800

    use opencc 1.0

--- src/PYSimpTradConverter.cc
+++ src/PYSimpTradConverter.cc
@@ -40,44 +40,26 @@ namespace PY {
 #ifdef HAVE_OPENCC
 
 class opencc {
-    static const int BUFFER_SIZE = 64;
 public:
     opencc (void)
     {
-        m_od = opencc_open (OPENCC_DEFAULT_CONFIG_SIMP_TO_TRAD);
-        g_assert (m_od != NULL);
+        m_cc = opencc_open (OPENCC_DEFAULT_CONFIG_SIMP_TO_TRAD);
+        g_assert (m_cc != NULL);
     }
 
     ~opencc (void)
     {
-        opencc_close(m_od);
+        opencc_close(m_cc);
     }
 
     void convert (const gchar *in, String &out)
     {
-        glong n_char;
-        gunichar *in_ucs4 = g_utf8_to_ucs4_fast (in, -1, &n_char);
-
-        ucs4_t *pinbuf = (ucs4_t *)in_ucs4;
-        size_t inbuf_left = n_char;
-        while (inbuf_left != 0) {
-            ucs4_t *poutbuf = (ucs4_t *)m_buffer;
-            size_t outbuf_left = BUFFER_SIZE;
-            size_t retval = opencc_convert(m_od, &pinbuf, &inbuf_left, &poutbuf, &outbuf_left);
-            if (retval == (size_t) -1) {
-                /* append left chars in pinbuf */
-                g_warning ("opencc_convert return failed");
-                out << (gunichar *) pinbuf;
-                break;
-            }
-            *poutbuf = L'\0';
-            out << m_buffer;
-        }
-        g_free (in_ucs4);
+        char * converted = opencc_convert_utf8 (m_cc, in, -1);
+        out = converted;
+        opencc_convert_utf8_free (converted);
     }
 private:
-    opencc_t m_od;
-    gunichar m_buffer[BUFFER_SIZE + 1];
+    opencc_t m_cc;
 };
 
 void
