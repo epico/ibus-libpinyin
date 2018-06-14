89e6d4c522217278ac24d5f4cfc38162a267c9a3
Peng Wu <alexepico@gmail.com>
2018-06-14 16:16:08 +0800

    write PYPSuggestionEditor.h

---
+++ src/PYPSuggestionEditor.h
@@ -0,0 +1,70 @@
+/* vim:set et ts=4 sts=4:
+ *
+ * ibus-libpinyin - Intelligent Pinyin engine based on libpinyin for IBus
+ *
+ * Copyright (c) 2018 Peng Wu <alexepico@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __PY_LIB_PINYIN_SUGGESTION_EDITOR_
+#define __PY_LIB_PINYIN_SUGGESTION_EDITOR_
+
+#include "PYEditor.h"
+#include "PYLookupTable.h"
+
+namespace PY {
+
+class SuggestionEditor : public Editor {
+public:
+    SuggestionEditor (PinyinProperties &props, Config & config);
+    virtual ~SuggestionEditor ();
+
+    virtual gboolean processKeyEvent (guint keyval, guint keycode, guint modifers);
+    virtual void pageUp (void);
+    virtual void pageDown (void);
+    virtual void cursorUp (void);
+    virtual void cursorDown (void);
+    virtual void update (void);
+    virtual void reset (void);
+    virtual void candidateClicked (guint index, guint button, guint state);
+
+private:
+    void clearLookupTable (void);
+    void updateLookupTable (void);
+    void updatePreeditText (void);
+    void updateAuxiliaryText (void);
+
+    gboolean selectCandidateInPage (guint index);
+    gboolean selectCandidate (guint index);
+
+    gboolean processSpace (guint keyval);
+    gboolean processEnter (guint keyval);
+    gboolean processEscape (guint keyval);
+
+private:
+    /* variables */
+    LookupTable m_lookup_table;
+
+    String m_preedit_text;
+    String m_auxiliary_text;
+
+    /* use LibPinyinBackEnd here. */
+    pinyin_instance_t           *m_instance;
+};
+
+};
+
+#endif
