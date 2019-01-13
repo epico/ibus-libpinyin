7c565e402369eaa82790b9f2550ec11ffd87e7ab
Peng Wu <alexepico@gmail.com>
2019-01-13 19:16:06 +0800

    fixes typo

--- src/PYFallbackEditor.cc
+++ src/PYFallbackEditor.cc
@@ -250,7 +250,7 @@ FallbackEditor::processKeyEvent (guint keyval, guint keycode, guint modifiers)
             retval = processPunct ('-', keycode, modifiers);
             break;
         case IBUS_KP_Decimal:
-            retval = False;
+            retval = FALSE;
             break;
         case IBUS_KP_Divide:
             retval = processPunct ('/', keycode, modifiers);
